e88126e7dabd900919a96aa8eef9e83cb245d8d3
Stephanie <Stephaniechloe21@gmail.com>
2023-05-25 07:47:52 -0700

    task 0

--- main_functions.c
+++ main_functions.c
@@ -19,6 +19,7 @@ void push(stack_t **stack, unsigned int __attribute__((unused)) line_number)
 	new = malloc(sizeof(stack_t));
 	if (new == NULL)
 	{
+		free(new);
 		free_stack(*stack);
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
