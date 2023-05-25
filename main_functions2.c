a910e58d25b0eb70094fc5c44fc1adac7e777a43
Stephanie <Stephaniechloe21@gmail.com>
2023-05-25 09:17:08 -0700

    sub

--- main_functions2.c
+++ main_functions2.c
@@ -33,3 +33,25 @@ void nop(stack_t **stack, unsigned int line_number)
 	(void) stack;
 	(void) line_number;
 }
+
+/**
+ * sub - function that subtracts the top two elements of the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: where the line number appears
+ * Description: 4. add
+ * Return: see below
+ * 1. upon success, nothing
+ * 2. upon fail, EXIT_FAILURE
+ */
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (stackLength < 2)
+		throw_error("can't sub, stack too short", line_number);
+
+	temp->prev->n -= temp->n;
+	pop(stack, line_number);
+}
+
